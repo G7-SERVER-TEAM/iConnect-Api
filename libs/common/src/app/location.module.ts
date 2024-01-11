a4b6de995edc955cded64c944a655c7a0a48662c
imwm-ns <narathip_aot@hotmail.com>
2024-01-11 22:18:50 +0700

    Updated a user, role, auth, account and area module.

--- libs/common/src/app/location.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AreaModule } from '../../../../apps/area/src/area/area.module';
-
-@Module({
-  imports: [AreaModule],
-})
-export class LocationModule {}
